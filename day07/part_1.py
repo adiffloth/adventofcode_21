ce0c45742ea2611d1c6966797b29592ae84f21f6
adiffloth <adiffloth@gmail.com>
2021-12-07 17:54:16 -0600

    Gaussian crabs

--- day07/part_1.py
+++ day07/part_1.py
@@ -5,5 +5,5 @@ crabs = [int(x) for x in open('day07/0.in').readline().split(',')]
 target = int(np.median(crabs))
 print(f'Part 1: {sum([abs(target - crab) for crab in crabs])}')
 
-target = int(np.mean(crabs))  # This rounds the wrong way for the worked example.
-print(f'Part 2: {sum([sum(range(abs(target - crab) + 1)) for crab in crabs])}')
+t = int(np.mean(crabs))  # int() vs round()
+print(f'Part 2: {sum([abs(t - c) * (abs(t - c) + 1) // 2 for c in crabs])}')  # Gauss
